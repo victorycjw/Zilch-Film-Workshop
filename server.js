607a2cf8fc5c5ed2814bd62d4ba34e511ce05fdb
Zilch <842570482@qq.com>
2019-07-15 17:43:29 +0800

    update
    更改了部分细节，准备仿写bilibili部分内容

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const userRouter=require('./routers/user.js');
 const myproRouter=require('./routers/mypro.js');
 //引入body-parser 中间件
 const bodyParser=require('body-parser');
-var app=express();
-app.listen(8080);
+var server=express();
+server.listen(8080);
 
 //托管静态资源到public目录下
-app.use( express.static('public') );
-app.use( express.static('mypro') );
-app.use( bodyParser.urlencoded({
+server.use( express.static('public') );
+server.use( express.static('mypro') );
+server.use( bodyParser.urlencoded({
     extended:false
     //不使用第三方qs模块，而是使用querystring模块
 }));
 
 //使用路由器
-app.use('/user.js',userRouter);
-app.use('/mypro',myproRouter);
+server.use('/user.js',userRouter);
+server.use('/mypro',myproRouter);
