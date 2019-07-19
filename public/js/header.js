758dd291630e6060854d65da86b51900a068474a
Zilch <51730945+victorycjw@users.noreply.github.com>
2019-07-19 19:18:05 +0800

    update and modify
    微调首页home.html，头尾用js引入，添加侧边栏和滑块

---
+++ public/js/header.js
@@ -0,0 +1,14 @@
+//js/header.js
+/*包含页头相关的所有代码*/
+$(function(){
+    $.ajax({
+        url:"header.html",
+        type:"get",
+        success:function(html){
+            // console.log(html)
+            $(html).replaceAll("#header");
+            $(`<link rel="stylesheet" href="css/header.css"/>`).appendTo("head");
+            $(`<link rel="stylesheet" href="css/common.css"/>`).appendTo("head");
+        }
+    })
+})
--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express=require('express');
 //引入用户路由器
-const userRouter=require('./routers/user.js');
-const myproRouter=require('./routers/mypro.js');
+const userRouter=require('./routes/user.js');
+const home=require('./routes/home.js');
+//const myproRouter=require('./routers/mypro.js');
 //引入body-parser 中间件
 const bodyParser=require('body-parser');
 var server=express();
-server.listen(8080);
+server.listen(3000);
 
 //托管静态资源到public目录下
 server.use( express.static('public') );
-server.use( express.static('mypro') );
+//server.use( express.static('mypro') );
 server.use( bodyParser.urlencoded({
     extended:false
     //不使用第三方qs模块，而是使用querystring模块
@@ -17,4 +18,5 @@ server.use( bodyParser.urlencoded({
 
 //使用路由器
 server.use('/user.js',userRouter);
-server.use('/mypro',myproRouter);
+server.use('/home.js',home);
+//server.use('/mypro',myproRouter);
